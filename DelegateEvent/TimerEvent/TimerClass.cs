8e4e21414efca4fd7b384051fece196510fab278
Nogin Sergey <ver-s_online@mail.ru>
2020-09-23 23:36:29 +0400

    init

---
+++ DelegateEvent/TimerEvent/TimerClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerEvent
+{
+    public class MyEventArgs:EventArgs
+    {
+        public int RemainigTime { get; private set; }
+
+        public void SetRemainigTime(int seconds)
+        {
+            RemainigTime = seconds;
+        }
+    }
+    public class TimerClass
+    {
+        public event EventHandler TimerStop;
+        public event EventHandler<MyEventArgs> RemainingTime;
+
+        public string Name { get; set; }
+        public int TimerTime { get; private set; }
+
+        /// <summary>
+        /// Timer class instance
+        /// </summary>
+        /// <param name="timerTime">Time in seconds</param>
+        public TimerClass(int timerTime)
+        {
+            TimerTime = timerTime;
+        }
+        /// <summary>
+        /// Timer class instance
+        /// </summary>
+        /// <param name="timerTime">Time in seconds</param>
+        /// <param name="name">Name timer</param>
+        public TimerClass(int timerTime, string name)
+        {
+            TimerTime = timerTime;
+            Name = name;
+        }
+
+        public void TimerStart()
+        {
+
+            MyEventArgs eventArgs = new MyEventArgs();
+            for (int i = 0; i < TimerTime; i++)
+            {
+                eventArgs.SetRemainigTime(TimerTime - i);
+                RemainingTime?.Invoke(this, eventArgs);
+
+                Thread.Sleep(1000);
+            }
+            TimerStop?.Invoke(this, null);
+        }
+    }
+}
---
+++ DelegateEvent/TimerUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimerEvent;
+
+namespace TimerUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("An application that demonstrates how the TimerEvent class works\nWaiting time in seconds: ");
+            
+            var input = Console.ReadLine();
+            if(!Int32.TryParse(input, out int time))
+            {
+                Console.WriteLine("Invalid data");
+                return;
+            }
+
+            TimerClass timer = new TimerClass(time, "TestTimer");
+            timer.TimerStop += Timer_TimerEvent;
+            timer.RemainingTime += Timer_RemainingTime;
+            timer.TimerStart();
+
+            Console.ReadLine();
+        }
+
+        private static void Timer_RemainingTime(object sender, EventArgs e)
+        {
+            Console.WriteLine($"There are now {((MyEventArgs)e).RemainigTime} seconds until site termination");
+        }
+
+        private static void Timer_TimerEvent(object sender, EventArgs e)
+        {
+            if(sender is TimerClass)
+            Console.WriteLine($"Timer {((TimerClass)sender).Name} stopped");
+        }
+    }
+}
